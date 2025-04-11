d13f7d91178cc9597a170cdcd5aed1f5a3eb4b5c
Abraham Putra Berman Simbolon <abrahamputrabs@gmail.com>
2025-04-11 21:30:36 +0700

    Mengumpulkan asesmen 1

--- app/src/main/java/com/abrahamputra0058/asesment1/navigation/NavGraph.kt
+++ app/src/main/java/com/abrahamputra0058/asesment1/navigation/NavGraph.kt
@@ -6,6 +6,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.abrahamputra0058.asesment1.ui.screen.AboutScreen
+import com.abrahamputra0058.asesment1.ui.screen.DetailAgendaScreen
 import com.abrahamputra0058.asesment1.ui.screen.MainScreen
 
 @Composable
@@ -16,6 +17,9 @@ fun SetupNavGraph(navController: NavHostController = rememberNavController()){
     ){
         composable(route = Screen.Home.route){
             MainScreen(navController)
+        }
+        composable(route = Screen.FormAdd.route){
+            DetailAgendaScreen(navController)
         }
         composable(route = Screen.About.route){
             AboutScreen(navController)
--- app/src/main/java/com/abrahamputra0058/asesment1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/abrahamputra0058/asesment1/ui/screen/MainScreen.kt
@@ -1,10 +1,24 @@
 package com.abrahamputra0058.asesment1.ui.screen
 
+import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
+import android.os.Message
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -13,14 +27,24 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.abrahamputra0058.asesment1.R
 import com.abrahamputra0058.asesment1.navigation.Screen
-import com.abrahamputra0058.asesment1.navigation.SetupNavGraph
+import com.abrahamputra0058.asesment1.ui.model.Agenda
+import com.abrahamputra0058.asesment1.ui.model.MainViewModel
 import com.abrahamputra0058.asesment1.ui.theme.Asesment1Theme
 
 @Composable
@@ -47,25 +71,101 @@ fun MainScreen(navController: NavHostController){
                     }
                 }
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {navController.navigate(Screen.FormAdd.route)}
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(R.string.add_agenda),
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
         }
+
     ) { innerPadding -> ScreenContent(Modifier.padding(innerPadding)) }
 }
 
+
 @Composable
-fun ScreenContent(modifier: Modifier){
-    Text(
-        text = "Hello Android!",
-        modifier = modifier
-    )
+fun ScreenContent(modifier: Modifier = Modifier){
+val viewModel: MainViewModel = viewModel()
+val data = viewModel.data
+
+        if (data.isEmpty()){
+            Column(
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = stringResource(id = R.string.empty_list))
+            }
+        } else {
+            LazyColumn(
+                modifier = modifier.fillMaxSize()
+            ) {
+                items(data) {
+                    ListItem(agenda = it)
+                    HorizontalDivider()
+                }
+            }
+
+//        Text(
+//            text = stringResource(R.string.intro_app),
+//            style = MaterialTheme.typography.bodyLarge,
+//            modifier = Modifier.padding(16.dp)
+//        )
+    }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ListItem(agenda: Agenda) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Image(
+            painter = painterResource(agenda.imageResId),
+            contentDescription = stringResource(R.string.image_name, agenda.tipe),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.size(24.dp)
+
+        )
+        Text(
+            text = agenda.judul,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+        )
+        Text(text = agenda.tipe)
+        Text(text = agenda.tanggal)
+        Text(text = agenda.waktu)
+        Text(
+            text = agenda.deskripsi,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+
+        )
+
+    }
 }
+
+
+
+//private fun shareAgenda(context: Context, message: String){
+//    val shareIntent = Intent(Intent.ACTION_SEND).apply {
+//        type = "text/plain"
+//        putExtra(Intent.EXTRA_TEXT, message)
+//    }
+//    if (shareIntent.resolveActivity(context.packageManager) != null) {
+//        context.startActivity(shareIntent)
+//    }
+//}
 
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
